61c77b72a8fc824bc3587f365b4e2d25b74298aa
Hson <nghongson9824@gmail.com>
2023-11-15 12:51:52 +0700

    Add project files.

---
+++ OOPcar/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPcar
+{
+    public class Person
+    {
+        private string name;
+        private string contactInfor;
+    }
+}
